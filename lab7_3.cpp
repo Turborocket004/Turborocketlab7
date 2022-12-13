1702d40fc1935701deb2c84ef5145d6b31e96658
Turborocket <Turborocket004@gmail.com>
2022-12-13 17:01:27 +0700

    lab_7.3done

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,36 @@
 #include<iostream>
 
 using namespace std;
-
+int adiff(int a,int b)
+{
+    int c,d;
+    if(a < b)
+        {
+            c = b - a;
+            d = c % 360;
+            if (d > 180)
+            {
+                return d = 360 - d;
+            }
+            else
+            {
+                return d;
+            }
+        }
+        else
+        {
+            c = a - b;
+            d = c % 360;
+            if(d > 180)
+            {
+                return d = 360 - d;
+            }
+            else
+            {
+                return d;
+            }
+        }
+    }    
 
 int main(){
   cout << adiff(180,270);
