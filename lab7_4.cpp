c93da8f7d919c9be0b429a5dbb5df6f85a8e62ae
Turborocket <Turborocket004@gmail.com>
2022-12-13 17:15:02 +0700

    lab_7.4done

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,20 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
-		
+   	
+	double key;
+	
+	cout << "Please input GPA: ";
+	cin >> key;
+	
+    int y = 0;
+    while(y<10){
+	if( key <= gpa[y]){
+	cout << id[y]<<" "<< name[y]<<" "<< gpa[y] << "\n" ;
+	}
+    
+    y++;
+	}	
 	return 0;
+
 }
